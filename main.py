913c6fae34601fecdedb3d5354f193c3bd7c7f9b
Samwise-B <sam.burcher99@gmail.com>
2023-08-27 00:50:36 +0100

    pulled on event off o2academy

--- main.py
+++ main.py
@@ -1,2 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
+
+website = "https://www.academymusicgroup.com/o2forumkentishtown/events/all"
+
+def getPageSoup(url):
+    res = requests.get(url)
+    content = res.text
+    soup = BeautifulSoup(content, "html.parser")
+    return soup
+
+def getEventList(soup):
+    mainDiv = soup.find(id="main")
+    eventList = mainDiv.find_all("li")
+    return eventList
+
+def parseEventList(eventList):
+    test = eventList[0]
+    textList = []
+    textList += test.find_all("span")
+    textList += test.find_all("h3")
+    textList += test.find_all("a")
+    for ele in textList:
+        print(ele.get_text())
+
+def main():
+    print("getting soup")
+    soup = getPageSoup(website)
+
+    print("getting event list")
+    eventList = getEventList(soup)
+
+    parseEventList(eventList)
+    print("done")
+
+if __name__ == '__main__':
+    main()
