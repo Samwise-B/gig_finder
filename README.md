23ca20d0dfbc7a79b47ebda1f9adc98349338967
Samwise-B <sam.burcher99@gmail.com>
2023-09-14 17:37:29 +0100

    general datetime parsing, api links & excel data storage

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # gig_finder
+
+## TODO
+- parse to pandas dataframe DONE
+- parse text
+- parse date & time
+
+o2 academy: Monday 26 February 2024 - 7.00pm (DONE)
+ jazz cafe: Monday 30th October (DONE)
+      KOKO: 9/11/2023 20:00:00
+roundhouse: Tue 12 Sep 23 (Done)
+ southbank: 2023-09-15T18:30:00Z (DONE)
+
+- get event links for api
+- abstract dynamic api urls
+- get genres from spotify?
--- main.py
+++ main.py
@@ -57,9 +57,24 @@ def get_tag(soup, cmds):
 def get_text_from_tag(tag):
     if tag != None:
         text = tag.get_text().strip()
+        text = re.sub("[\n+, \t+]", " ", text)
+        text = re.sub("  +", ": ", text)
     else:
-        text = "unavailable"
+        text = None
     return text
+
+def parse_date(date, format):
+    if date == None:
+        return None
+    if format != None:
+        pattern = r'(?<=\d)[t, s, r, n][h, t, d, d]'
+        date = re.sub(pattern, "", date)
+        date = datetime.datetime.strptime(date, format)
+        if date.year == 1900:
+            date = date.replace(year=datetime.datetime.today().year)
+        return date
+    else:
+        return date
 
 def get_event_links(soup, cmds, baseUrl, relative):
     aTags = get_tags(soup, cmds)
@@ -111,32 +126,65 @@ def parse_html(pageObj, cookies):
         eventLinks = get_event_links(soup, pageObj['getLinks'], pageObj['base-url'], False)
 
     print("scraping links")
+    events = []
     # for each link
     for eventLink in eventLinks:
         # scrape event page
         soup = get_page_soup(eventLink, cookies, pageObj['dynamic'])
+        row = []
 
+        # get data
         for (field, data) in pageObj['htmlTags'].items():
-            tag = get_tag(soup, data)
-            if field == "link" and tag != None:
-                tag.get('href')
+            if data != None:
+                tag = get_tag(soup, data)
+                if field == "link" and tag != None:
+                    tag.get('href')
+                elif field == "datetime":
+                    datetime = parse_date(get_text_from_tag(tag), pageObj['date-format'])
+                    if datetime != None and not isinstance(datetime, str):
+                        date = datetime.strftime("%m/%d/%Y")
+                        time = datetime.strftime("%H:%M:%S")
+                        row.append(time)
+                        row.append(date)
+                    else:
+                        row.append(None)
+                        row.append(None)
+                elif field == "time":
+                    time = parse_date(get_text_from_tag(tag), pageObj['time-format'])
+                    if time != None and not isinstance(time, str):
+                        time = time.strftime("%H:%M")
+                    row.append(time)
+                elif field == "date":
+                    date = parse_date(get_text_from_tag(tag), pageObj['date-format'])
+                    if date != None and not isinstance(date, str):
+                        date = date.strftime("%m/%d/%Y")
+                    row.append(date)
+                else:
+                    print(f"{field}: {get_text_from_tag(tag)}")
+                    row.append(get_text_from_tag(tag))
             else:
-                print(f"{field}: {get_text_from_tag(tag)}")
+                row.append(None)
+        
+        row.append(eventLink)
+        events.append(row)
+    
+    return events
 
 def convert_string_to_json(elem):
     return json.loads(elem)
 
-def parse_date(data, type):
-    return "TODO"
-
 def get_dict_element(data, refs):
     elem = data
     for i in range(len(refs)):
-        if refs[i] == "string-to-json":
+        if elem == "":
+            return None
+        elif refs[i] == "string-to-json":
             elem = convert_string_to_json(elem)
         elif refs[i] == "milliseconds":
             elem = datetime.datetime.fromtimestamp(int(re.sub('\D', '', elem)) / 1000)
         elif isinstance(refs[i], int):
+            if len(elem) == 0:
+                return None
             elem = elem[refs[i]]
         else:
             elem = elem[refs[i]]
@@ -148,11 +196,15 @@ def get_dict_elements(data, refs):
     for arr in refs:
         elem = data
         for i in range(len(arr)):
-            if arr[i] == "string-to-json":
+            if elem == "":
+                return None
+            elif arr[i] == "string-to-json":
                 elem = convert_string_to_json(elem)
             elif arr[i] == "milliseconds":
                 elem = datetime.datetime.fromtimestamp(re.sub('\D', '', elem) / 1000)
             elif isinstance(arr[i], int):
+                if len(elem) == 0:
+                    return None
                 elem = elem[arr[i]]
             else:
                 elem = elem[arr[i]]
@@ -191,28 +243,59 @@ def parse_api(page_obj, cookies):
     print("received api json")
 
     eventList = get_dict_element(json_data, page_obj['event-list'])
-
+    events = []
     for event in eventList:
+        row = []
         if page_obj['condition'] == True:
             if get_dict_element(event, page_obj['event-type'][0]) != page_obj['event-type'][1]:
                 continue
         for field, data in page_obj['data'].items():
-            if field == "genres":
-                print(f"{field}: {get_dict_elements(event, data)}")
+            if data != None:
+                if field == "genres":
+                    print(f"{field}: {get_dict_elements(event, data)}")
+                    row.append(get_dict_elements(event, data))
+                elif field == "datetime":
+                    datetime = parse_date(get_dict_element(event, data), page_obj['date-format'])
+                    if datetime != None and not isinstance(datetime, str):
+                        date = datetime.strftime("%m/%d/%Y")
+                        time = datetime.strftime("%H:%M:%S")
+                        row.append(time)
+                        row.append(date)
+                    else:
+                        row.append(None)
+                        row.append(None)
+                elif field == "time":
+                    time = parse_date(get_dict_element(event, data), page_obj['time-format'])
+                    if time != None and not isinstance(time, str):
+                        time = time.strftime("%H:%M")
+                    row.append(time)
+                elif field == "date":
+                    date = parse_date(get_dict_element(event, data), page_obj['date-format'])
+                    if date != None and not isinstance(date, str):
+                        date = date.strftime("%m/%d/%Y")
+                    row.append(date)
+                elif field == "link":
+                    if page_obj['relative'] == True:
+                        row.append(page_obj['base-url'] + get_dict_element(event, data))
+                    else:
+                        row.append(get_dict_element(event, data))
+                else:
+                    print(f"{field}: {get_dict_element(event, data)}")
+                    row.append(get_dict_element(event, data))
             else:
-                print(f"{field}: {get_dict_element(event, data)}")
+                row.append(None)
 
-        print("Done!")
+        events.append(row)
+
+    return events
 
 def main():
     pageObjects = []
     with open("websites.json", "r") as fp:
         pageObjects = json.load(fp)
-
-    eventsTable = pd.DataFrame()
-    eventsTable = eventsTable.assign(event_title=[], time=[], date=[], price=[], status=[], link=[])
     
-    for pageObj in pageObjects[4:]:
+    events = []
+    for pageObj in pageObjects:
         cookies = pageObj['cookies']
         # check for cookie manage
         if pageObj['cookiesRequired'] == True:
@@ -220,10 +303,13 @@ def main():
             cookies = parseCookieTemplate(cookies, cookie_for_requests)
         
         if pageObj['type'] == "html":
-            parse_html(pageObj, cookies)
+            events += parse_html(pageObj, cookies)
         if pageObj['type'] == "api":
-            parse_api(pageObj, cookies)
-        
+            events += parse_api(pageObj, cookies)
+
+    df = pd.DataFrame(events, columns=['title', "time", "date", "price", "genres", "status", "link"])
+    
+    df.to_excel("events.xlsx", sheet_name="events_table")
 
     print("Done!")
     return 0
